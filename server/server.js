e63c4fcebcf923ed992d4807f925e68c14f259a0
Max Kotyk <mxm.kotyk@gmail.com>
2024-03-01 18:46:28 +0100

    Created server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,21 @@
-console.log("Server is running");
+const path = require("path");
+const configPath = path.join(__dirname, "..", "config", ".env");
+const express = require("express");
+
+require("dotenv").config({ path: configPath });
+require("colors");
+const connectDb = require("../config/connectDb");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Connected");
+});
+
+const { PORT } = process.env;
+
+connectDb();
+
+app.listen(PORT, () => {
+  console.log(`Server is listening the port: ${PORT}`);
+});
