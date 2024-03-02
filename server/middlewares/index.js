94d04424cc7ff5cc13a6fc67ab5ed42f79b650b9
Max Kotyk <mxm.kotyk@gmail.com>
2024-03-02 21:48:27 +0100

    Added middlewares

---
+++ server/middlewares/index.js
@@ -0,0 +1,7 @@
+const notFoundUrl = require("./notFoundUrl");
+const errorHandler = require("./errorHandler");
+
+module.exports = {
+  notFoundUrl,
+  errorHandler,
+};
---
+++ server/middlewares/notFoundUrl.js
@@ -0,0 +1,7 @@
+module.exports = (req, res) => {
+  res.status(404);
+  res.json({
+    code: 404,
+    message: "URL not found",
+  });
+};
