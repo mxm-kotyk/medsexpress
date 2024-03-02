3904c8de568fd9ad11f8c267590cf5b009831ba1
Max Kotyk <mxm.kotyk@gmail.com>
2024-03-02 21:51:02 +0100

    Added endpoint handlers and middlewares to server

--- server/server.js
+++ server/server.js
@@ -5,14 +5,22 @@ const express = require("express");
 require("dotenv").config({ path: configPath });
 require("colors");
 const connectDb = require("../config/connectDb");
+const { drugsRoutes, storesRoutes, ordersRoutes } = require("./routes");
+const { notFoundUrl, errorHandler } = require("./middlewares");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Connected");
-});
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use("/api/drugs", drugsRoutes);
+app.use("/api/stores", storesRoutes);
+app.use("/api/orders", ordersRoutes);
 
 const { PORT } = process.env;
+
+app.use(notFoundUrl);
+app.use(errorHandler);
 
 connectDb();
 
