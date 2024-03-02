0833558b80c57852c0af065e8e69dc7fbae383fd
Max Kotyk <mxm.kotyk@gmail.com>
2024-03-02 22:36:40 +0100

    Added body validation middleware

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -1,7 +1,9 @@
 const notFoundUrl = require("./notFoundUrl");
 const errorHandler = require("./errorHandler");
+const validateBody = require("./validateBody");
 
 module.exports = {
   notFoundUrl,
   errorHandler,
+  validateBody,
 };
---
+++ server/middlewares/validateBody.js
@@ -0,0 +1,11 @@
+module.exports = (schema) => {
+  return (req, res, next) => {
+    const { error } = schema.validate(req.body);
+    if (error) {
+      const errorMessage = error.details[0].message;
+      res.status(400);
+      throw new Error(errorMessage);
+    }
+    next();
+  };
+};
