f7eb6356804de96b7e590693b33d75637e078dab
Max Kotyk <mxm.kotyk@gmail.com>
2024-03-04 02:44:14 +0100

    Added loader

---
+++ client/src/components/Loader/Loader.jsx
@@ -0,0 +1,11 @@
+import { LoaderWrapper, StyledLoader } from "./Loader.styled";
+
+const Loader = () => {
+  return (
+    <LoaderWrapper>
+      <StyledLoader></StyledLoader>
+    </LoaderWrapper>
+  );
+};
+
+export default Loader;
