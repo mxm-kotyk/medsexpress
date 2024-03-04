4a1cbc0fba98cf0b14e6a7c3148fc23ce226674b
Max Kotyk <mxm.kotyk@gmail.com>
2024-03-04 02:47:12 +0100

    Added loaders

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import { useGetDrugsQuery, useGetStoresQuery } from "../../redux/api";
+import { useGetDrugsQuery, useGetStoresQuery } from "../redux/api";
 import Section from "components/Section";
 import Container from "components/Container";
 import ContentWrapper from "components/ContentWrapper";
@@ -8,8 +8,10 @@ import CardsGrid from "components/CardsGrid";
 import Card from "components/Card/Card";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addToOrder, incrementQuantity } from "../../redux/orderSlice";
-import { getOrderItems } from "../../redux/selectors";
+import { addToOrder, incrementQuantity } from "../redux/orderSlice";
+import { getOrderItems } from "../redux/selectors";
+import { Notify } from "notiflix";
+import Loader from "components/Loader";
 
 const filterDrugs = (selectedStore, drugs) => {
   if (selectedStore === "") return drugs;
@@ -17,8 +19,8 @@ const filterDrugs = (selectedStore, drugs) => {
 };
 
 const HomePage = () => {
-  const { data: stores } = useGetStoresQuery();
-  const { data: drugs } = useGetDrugsQuery();
+  const { data: stores, isLoading: isLoadingStores } = useGetStoresQuery();
+  const { data: drugs, isLoading: isLoadingDrugs } = useGetDrugsQuery();
   const dispatch = useDispatch();
   const orderItems = useSelector(getOrderItems);
   const [selectedStore, setSelectedStore] = useState("");
@@ -37,6 +39,9 @@ const HomePage = () => {
     } else {
       dispatch(incrementQuantity(_id));
     }
+    Notify.success(`${name} was added to your order.`, {
+      position: "center-top",
+    });
   };
 
   return (
@@ -45,6 +50,7 @@ const HomePage = () => {
         <Container>
           <ContentWrapper>
             <h2>Shops:</h2>
+            {isLoadingStores && <Loader />}
             <ShopsList>
               {stores &&
                 stores.map((store) => (
@@ -57,6 +63,7 @@ const HomePage = () => {
             </ShopsList>
           </ContentWrapper>
           <ContentWrapper>
+            {isLoadingDrugs && <Loader />}
             <CardsGrid>
               {drugs &&
                 filteredDrugs.map((drug) => (
