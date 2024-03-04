8aaf0d5061e5be420e91315322ae9f07d2962404
Max Kotyk <mxm.kotyk@gmail.com>
2024-03-04 02:46:55 +0100

    Finished ShoppingCartPage

--- client/src/pages/ShoppingCartPage.jsx
+++ client/src/pages/ShoppingCartPage.jsx
@@ -1,5 +1,38 @@
+import Container from "components/Container";
+import ContentWrapper from "components/ContentWrapper";
+import OrderForm from "components/OrderForm";
+import OrderItemCard from "components/OrderItemCard";
+import OrderItemsList from "components/OrderItemsList";
+import Section from "components/Section";
+import { useSelector } from "react-redux";
+import { getOrderItems } from "../redux/selectors";
+
 const ShoppingCartPage = () => {
-  return <div>Shopping Cart</div>;
+  const orderItems = useSelector(getOrderItems);
+
+  const emptyOrderMessage =
+    "Your order list is empty. Add some items to cart and they will appear here.";
+
+  return (
+    <>
+      <Section>
+        <Container>
+          <ContentWrapper>
+            <OrderForm />
+          </ContentWrapper>
+          <ContentWrapper>
+            <OrderItemsList>
+              {orderItems.length > 0
+                ? orderItems.map((item) => (
+                    <OrderItemCard key={item._id} orderItem={item} />
+                  ))
+                : emptyOrderMessage}
+            </OrderItemsList>
+          </ContentWrapper>
+        </Container>
+      </Section>
+    </>
+  );
 };
 
 export default ShoppingCartPage;
